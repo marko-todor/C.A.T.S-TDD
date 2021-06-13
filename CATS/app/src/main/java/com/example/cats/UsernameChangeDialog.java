8f1a2d5141a9de663619e41da0df25af09a79599
marko-todor <48258536+marko-todor@users.noreply.github.com>
2021-06-13 19:18:18 +0200

    Implemented update username

--- CATS/app/src/main/java/com/example/cats/UsernameChangeDialog.java
+++ CATS/app/src/main/java/com/example/cats/UsernameChangeDialog.java
@@ -31,6 +31,45 @@ public class UsernameChangeDialog {
             }
         });
 
+        Button save_changes = dialog.findViewById(R.id.button_save_new_username);
+
+        save_changes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView new_username = dialog.findViewById(R.id.new_username_edit);
+                final String newUsername = new_username.getText().toString();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        User user = ((MainActivity) activity).db.userDao().findByName(newUsername);
+                        if (user == null) {
+                            ((MainActivity) activity).db.userDao().updateUsername(model.loggedIn, newUsername);
+                            List<String> users = model.getUsersLiveData().getValue();
+                            for (int i = 0; i < users.size(); i++) {
+                                if (users.get(i).equals(model.loggedIn)) {
+                                    users.set(i, newUsername);
+                                }
+                            }
+                            model.getUsersLiveData().postValue(users);
+                            model.loggedIn = newUsername;
+                            dialog.dismiss();
+                        } else {
+                            final TextView error = dialog.findViewById(R.id.textView_username_error);
+                            error.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    error.setText("Username " + newUsername + " is already taken");
+                                }
+                            });
+
+                        }
+                    }
+                }).start();
+
+            }
+        });
+
         Button dialogButton = (Button) dialog.findViewById(R.id.btn_dialog);
         dialogButton.setOnClickListener(new View.OnClickListener() {
             @Override
